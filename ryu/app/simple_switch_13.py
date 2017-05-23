2cd1cb7939974fe71b4712f6c85ae25d1dfd169c
ustbgaofan <ustbgaofan@gmail.com>
2017-05-23 15:45:56 +0800

    Update simple_switch_13.py

--- ryu/app/simple_switch_13.py
+++ ryu/app/simple_switch_13.py
@@ -60,9 +60,13 @@ class SimpleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         
+        //Instruction 是定义当封包满足match时，所要执行的动作
+        //因此把action以 OFPInstructionActions 包装起来
         
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
+        
+        // FlowMod Function 可以让我们对Switch写入由我们所定义的Flow Entry        
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
                                     priority=priority, match=match,
@@ -70,6 +74,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         else:
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                     match=match, instructions=inst)
+         // 把定义好的FlowEntry送给Switch
         datapath.send_msg(mod)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -79,10 +84,14 @@ class SimpleSwitch13(app_manager.RyuApp):
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
+        //收到来自Switch 不知如何处理的封包（Match 到 Table-Miss FlowEntry）
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
+        //in_port 相当于分包从Switch 的哪个port 进到Switch 中
+        //同事也代表 source Host MAC 要往 in_port 送，才能送达
+        
         in_port = msg.match['in_port']
 
         pkt = packet.Packet(msg.data)
@@ -91,24 +100,37 @@ class SimpleSwitch13(app_manager.RyuApp):
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
             return
-        dst = eth.dst
-        src = eth.src
+        
+        dst = eth.dst  //得到封包目的端MAC address
+        src = eth.src  //得到封包来源端MAC address 
 
-        dpid = datapath.id
+        dpid = datapath.id    //Switch 的 datapath id （独一无二的ID）
+        
+        //如果 MAC表内不曾存储过这个Switch的MAC， 则帮他新增一个预设值
+        // ex. mac_to_port = {'1': {'AA:BB:CC:DD:EE:FF': 2}}
+        //    但是目前 dpid 为 2 不存在，执行 mac_to_port 会变成
+        //    mac_to_port = {'1': {'AA:BB:CC:DD:EE:FF':2}, '2': {}}
         self.mac_to_port.setdefault(dpid, {})
 
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
-
+        
+        //我们拥有来源端MAC 与in_port 了，因此可以学习到src MAC要往 in_port 送
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
 
+        //如果 目的端MAC 在 mac_to_port 表中的话，就直接告诉Switch送到 out_port
+        //否则就请 Switch 用Flooding送出去
+        
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
             out_port = ofproto.OFPP_FLOOD
 
+        //把刚刚的out_port 做成这次风暴的的处理动作
         actions = [parser.OFPActionOutput(out_port)]
-
+        
+        //如果没有让switch flooding，表示目的端mac 有学习过
+        //因此使用 add_flow 让Switch 新增 FlowEntry 学习此条规则
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
             match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
@@ -122,7 +144,8 @@ class SimpleSwitch13(app_manager.RyuApp):
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
-
+            
+        //把要Switch执行的动作包装成Packet_out， 并让Switch执行动作
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
